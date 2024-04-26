fea62ce4894f800b3172b9f09e06efdb040e88f7
Abhishek Sinha <abhishek.sinha132@gmail.com>
2024-04-26 16:30:06 +0530

    feature: model integration

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pickle
+
+# @st.cache_data(allow_output_mutation=True)
+def load_model(file_path):
+    with open(file_path, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+# Define the file path to your .pkl file
+file_path = './Embeddings/faiss_index_all-mpnet-base-v2/index.pkl'
+
+# Load the model
+model = load_model(file_path)
 
 # Set page title and background color
 st.set_page_config(page_title="DocTalk", page_icon=":memo:", layout="wide", initial_sidebar_state="collapsed")
@@ -18,7 +31,6 @@ custom_styles = """
 </style>
 """
 
-# Display custom CSS styles
 st.markdown(custom_styles, unsafe_allow_html=True)
 
 # Title
@@ -34,8 +46,14 @@ with st.form(key='my_form'):
     submit_button = st.form_submit_button(label='Submit')
 
 # Handle form submission
+## Assuming the model is a classifier
+
 if submit_button:
-  
+    # Perform prediction using the loaded model
+    st.write(type(model))
+    prediction = model.predict([feedback])[0]
+    st.write(f"Prediction: {prediction}")
+
     st.write("Here is your answer:")
  
     result = st.text_area("", "Do proper exercise, you will be just fine!")
